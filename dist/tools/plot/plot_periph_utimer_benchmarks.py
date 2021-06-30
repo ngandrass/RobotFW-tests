3a8c58e08e1ee1c4778d2599aaaf51736859e8a7
ngandrass <ngandrass@squacu.de>
2021-06-30 09:22:30 +0200

    plot_periph_utimer_benchmarks: Compare timer write and read operations between APIs

--- dist/tools/plot/plot_periph_utimer_benchmarks.py
+++ dist/tools/plot/plot_periph_utimer_benchmarks.py
@@ -13,6 +13,7 @@ import plotly.express as px
 
 LOG = logging.getLogger(__name__)
 
+
 class FigurePlotter:
 
     def __init__(self, testsuite_name, xunit_file, outdir):
@@ -42,6 +43,24 @@ class FigurePlotter:
             self.board
         ))
 
+    def _extract_bench_values_from_json(self, bench_props, values_key="values"):
+        values = []
+        for traceset_json in bench_props:
+            traceset = json.loads(traceset_json.replace("'", "\""))
+            values = values + traceset[values_key]
+
+        return values
+
+    def _calc_statistical_properties(self, dataset):
+        return {
+            'max': np.max(dataset),
+            'min': np.min(dataset),
+            'avg': np.average(dataset),
+            'mean': np.mean(dataset),
+            'stdev': np.std(dataset),
+            'samples': len(dataset)
+        }
+
     def _save_figure_as_html(self, fig, title):
         outfile = "{}/{}.html".format(self.outdir, title)
         fig.write_html(
@@ -53,24 +72,10 @@ class FigurePlotter:
         LOG.info("Wrote figure: {}".format(outfile))
 
     def plot_gpio_latency(self):
+        # Process samples
         BENCH_NAME = 'bench_gpio_latency'
-
-        # Combine trace samples
-        durations = []
-        for traceset_json in self.benchmarks['Measure GPIO Latency'][BENCH_NAME]:
-            traceset = json.loads(traceset_json.replace("'", "\""))
-            durations = durations + traceset['values']
-
-        # Calculate statistical properties
-        stats = {
-            'max': np.max(durations),
-            'min': np.min(durations),
-            'avg': np.average(durations),
-            'mean': np.mean(durations),
-            'stdev': np.std(durations),
-            'samples': len(durations)
-    }
-        LOG.info("GPIO Latency: {}".format(stats))
+        durations = self._extract_bench_values_from_json(self.benchmarks['Measure GPIO Latency'][BENCH_NAME])
+        LOG.info("GPIO Latency: {}".format(self._calc_statistical_properties(durations)))
 
         # Generate box plot
         fig = px.box(
@@ -86,6 +91,35 @@ class FigurePlotter:
         )
 
         self._save_figure_as_html(fig, BENCH_NAME)
+
+    def plot_read_write_ops(self):
+        # Read, combine and process trace samples
+        reads_uapi = self._extract_bench_values_from_json(self.benchmarks['Benchmark uAPI Timer Read']['bench_timer_read_uapi'])
+        reads_hapi = self._extract_bench_values_from_json(self.benchmarks['Benchmark hAPI Timer Read']['bench_timer_read_hapi'])
+        writes_uapi = self._extract_bench_values_from_json(self.benchmarks['Benchmark uAPI Timer Write']['bench_timer_write_uapi'])
+        writes_hapi = self._extract_bench_values_from_json(self.benchmarks['Benchmark hAPI Timer Write']['bench_timer_write_hapi'])
+
+        LOG.info("Benchmark uAPI Timer Read: {}".format(self._calc_statistical_properties(reads_uapi)))
+        LOG.info("Benchmark hAPI Timer Read: {}".format(self._calc_statistical_properties(reads_hapi)))
+        LOG.info("Benchmark uAPI Timer Write: {}".format(self._calc_statistical_properties(writes_uapi)))
+        LOG.info("Benchmark uAPI Timer Write: {}".format(self._calc_statistical_properties(writes_hapi)))
+
+        # Generate plot
+        data = pd.DataFrame({
+            'Read (uAPI)':  reads_uapi,
+            'Read (hAPI)':  reads_hapi,
+            'Write (uAPI)': writes_uapi,
+            'Write (hAPI)': writes_hapi
+        })
+
+        fig = px.box(data_frame=data, points="outliers")
+        fig.update_layout(
+            title="Timer Read and Write Operations - Board: {}".format(self.board),
+            yaxis_title="Execution time",
+            yaxis_ticksuffix="s",
+            xaxis_title="Operation (via API)"
+        )
+        self._save_figure_as_html(fig, "bench_timer_read_write")
 
 
 def main():
@@ -107,6 +141,7 @@ def main():
     # Generate plots
     plotter = FigurePlotter(testsuite_name="tests_periph_utimer_benchmarks", xunit_file=args.input, outdir=args.outdir)
     plotter.plot_gpio_latency()
+    plotter.plot_read_write_ops()
 
 
 if __name__ == "__main__":
