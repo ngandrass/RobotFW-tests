54b8c29b70a2150e3d74fb65c5d2960fba565511
Niels Gandraß <niels@gandrass.de>
2021-11-04 17:17:38 +0100

    Process parallel callbacks benchmark in plotting script

--- dist/tools/plot/plot_periph_utimer_benchmarks.py
+++ dist/tools/plot/plot_periph_utimer_benchmarks.py
@@ -474,6 +474,73 @@ class FigurePlotter:
         if self.dump_data:
             self._dump_dataframe_to_csv(df, title)
 
+    def plot_board_parallel_callback_latencies(self, board):
+        # Read, combine and process trace samples
+        timeouts = []
+        freq = None
+        timeout = None
+        for testsuite, testsuite_data in self.benchmarks[board].items():
+            for case, data in testsuite_data['benchmarks'].items():
+                if case.startswith('Benchmark Parallel Callbacks'):
+                    if not data:
+                        continue
+
+                    case_timeout = int(data['ticks'][0])/int(data['frequency'][0])
+                    freq = int(data['frequency'][0])
+                    timeout = case_timeout
+                    for duration in self._extract_bench_values_from_json(data['bench_parallel_callbacks']):
+                        duration = duration - self._get_gpio_latency(board)
+                        timeouts.append({
+                            'api': testsuite_data['api'],
+                            'frequency': int(data['frequency'][0]),
+                            'ticks': int(data['ticks'][0]),
+                            'channels': int(data['channels'][0]),
+                            'timeout': case_timeout,
+                            'duration': duration,
+                            'latency': duration - case_timeout
+                        })
+
+        if not timeouts:
+            LOG.error(f"No data for parallel callback timeouts for: board={board}")
+            return
+
+        df = pd.DataFrame(timeouts)
+
+        # Calculate statistical properties
+        for api in df['api'].unique():
+            for channels in df[df['api'] == api]['channels'].unique():
+                LOG.info("Benchmark Parallel Callbacks: board={}, api={}, freq={}Hz, timeout={}s, channels={}: {}".format(
+                    board,
+                    api,
+                    si_format(freq, precision=0),
+                    si_format(timeout, precision=0),
+                    channels,
+                    self._calc_statistical_properties(df[(df['api'] == api) & (df['channels'] == channels)]['latency'])
+                ))
+
+        # Plot latencies of latest callback latency for each benchmarked number of parallel channels
+        fig = px.box(
+            data_frame=df,
+            x=[channels for channels in df['channels']],
+            y='latency',
+            color='api',
+            points="outliers"
+        )
+        fig.update_layout(
+            title=f"Parallel Callback Execution Latency - Board: {board}, Timeout: {si_format(timeout, precision=0)}s, Frequency: {si_format(freq, precision=0)}Hz",
+            yaxis_title="Timeout Latency",
+            yaxis_ticksuffix="s",
+            xaxis_title="Parallel Timeouts",
+            xaxis_ticksuffix="",
+            xaxis_showgrid=True,
+            yaxis_showgrid=True
+        )
+
+        title = f"{board}_bench_parallel_callbacks"
+        self._save_figure(fig, title)
+        if self.dump_data:
+            self._dump_dataframe_to_csv(df, title)
+
     ######################
     ### Overview plots ###
     ######################
@@ -708,6 +775,91 @@ class FigurePlotter:
         if self.dump_data:
             self._dump_dataframe_to_csv(df, title)
 
+    def plot_parallel_callbacks(self, channels):
+        # Process samples into DataFrame
+        samples = []
+        timeout = None
+        freq = None
+        for board, suites in self.benchmarks.items():
+            for suite, suite_data in suites.items():
+                for bench_name, bench_data in suite_data['benchmarks'].items():
+                    if bench_name.startswith('Benchmark Parallel Callbacks'):
+                        if not bench_data:
+                            continue
+
+                        if int(bench_data['channels'][0]) == channels:
+                            freq = int(bench_data['frequency'][0])
+                            timeout = int(bench_data['ticks'][0])/int(bench_data['frequency'][0])
+                            durations = self._get_benchmark_data(board, suite, bench_name, 'bench_parallel_callbacks')
+                            for duration in durations:
+                                samples.append({
+                                    'board': board,
+                                    'api': suite_data['api'],
+                                    'frequency': freq,
+                                    'timeout': timeout,
+                                    'channels': channels,
+                                    'duration': duration - self._get_gpio_latency(board),
+                                    'latency': duration - timeout - self._get_gpio_latency(board)
+                                })
+
+        if len(samples) == 0:
+            return
+
+        df = pd.DataFrame(samples)
+
+        # Calculate statistical properties
+        for board in df['board'].unique():
+            for api in df[df['board'] == board]['api'].unique():
+                LOG.info("Benchmark Parallel Callbacks {}x {}s @ {}Hz on board={} for api={}: {}".format(
+                    channels,
+                    si_format(timeout, precision=0),
+                    si_format(freq, precision=0),
+                    board,
+                    api,
+                    self._calc_statistical_properties(df[(df['api'] == api) & (df['board'] == board)]['latency'])
+                ))
+
+        # Plot parallel callback latencies
+        fig = px.box(
+            data_frame=df,
+            x='board',
+            y='latency',
+            color='api',
+            points="outliers",
+            **self.PLOTLY_COMMON_PLOT_PROPS
+        )
+        fig.update_traces(
+            marker=dict(opacity=0),  # Detect but hide outliers
+            line=dict(width=1)
+        )
+        fig.update_layout(
+            title=dict(
+                text=f"Parallel Callback Execution Latency - {channels} Channel(s) x {si_format(timeout, precision=0)}s @ {si_format(freq, precision=0)}Hz",
+                y=1
+            ),
+            legend=dict(
+                title="",
+                orientation="h",
+                yanchor="bottom",
+                y=1.01,
+                xanchor="right",
+                x=0.99,
+            ),
+            yaxis_title="Callback Execution Latency",
+            yaxis_ticksuffix="s",
+            xaxis_title="Board",
+            xaxis_ticksuffix="",
+            xaxis_showgrid=True,
+            yaxis_showgrid=True,
+            yaxis_rangemode="tozero",
+            **self.PLOTLY_COMMON_LAYOUT_PROPS
+        )
+
+        title = f"overview_parallel_callbacks_{channels}x"
+        self._save_figure(fig, title)
+        if self.dump_data:
+            self._dump_dataframe_to_csv(df, title)
+
 
 def main():
     # Configure logging
@@ -750,6 +902,9 @@ def main():
         for ticks in [1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9]:
             plotter.plot_timeout_latencies(freq=freq, ticks=ticks)
 
+    for channels in range(1, 9):
+        plotter.plot_parallel_callbacks(channels)
+
     # Board specific plots
     for board in plotter.get_boards():
         plotter.plot_board_gpio_latency(board)
@@ -768,6 +923,8 @@ def main():
         plotter.plot_board_absolute_timeouts_grouped_by_timeout(board, timeout=1e-2)
         plotter.plot_board_absolute_timeouts_grouped_by_timeout(board, timeout=1e-1)
         plotter.plot_board_absolute_timeouts_grouped_by_timeout(board, timeout=1e-0)
+
+        plotter.plot_board_parallel_callback_latencies(board)
 
 
 if __name__ == "__main__":
